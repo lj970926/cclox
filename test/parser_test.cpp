981106590f660f50bafd2b814e33d3ba08d14c50
lj970926 <1783973490@qq.com>
2023-01-12 21:29:20 +0800

    feat: add more test for parser

--- test/parser_test.cpp
+++ test/parser_test.cpp
@@ -28,8 +28,79 @@ TEST(ParserTest, BasicTest) {
 
   Parser parser(tokens, reporter);
   ExprPtr expr = parser.Parse();
+  ASSERT_EQ(reporter.status(), LoxStatus::OK);
 
   AstPrinter printer;
   ASSERT_EQ(printer.Print(*expr.get()), printer.Print(*expected.get()));
+}
+
+TEST(ParserTest, MultipleToken) {
+  std::string code = "(-3 - 2) * 8\n";
+  ExprPtr expected = std::make_unique<BinaryExpr>(
+      std::make_unique<GroupingExpr>(
+          std::make_unique<BinaryExpr>(
+              std::make_unique<UnaryExpr>(
+                  Token(TokenType::MINUS, "-", std::nullopt, 1),
+                  std::make_unique<LiteralExpr>(3.0)
+                  ),
+              Token(TokenType::MINUS, "-", std::nullopt, 1),
+              std::make_unique<LiteralExpr>(2.0)
+              )
+          ),
+      Token(TokenType::STAR, "*", std::nullopt, 1),
+      std::make_unique<LiteralExpr>(8.0)
+      );
+
+  ErrorReporter reporter;
+  Scanner scanner(code, reporter);
+  auto tokens = scanner.ScanTokens();
+  ASSERT_EQ(tokens.size(), 9);
+
+  Parser parser(tokens, reporter);
+  ExprPtr expr = parser.Parse();
+  ASSERT_EQ(reporter.status(), LoxStatus::OK);
+
+  AstPrinter printer;
+
+  ASSERT_EQ(printer.Print(*expr.get()), printer.Print(*expected.get()));
+}
+
+TEST(ParserTest, EqualCompAndLiteral) {
+  std::string code = "(\"a\" > 3) == true\n";
+  ExprPtr expected = std::make_unique<BinaryExpr>(
+      std::make_unique<GroupingExpr>(
+          std::make_unique<BinaryExpr>(
+              std::make_unique<LiteralExpr>("a"),
+              Token(TokenType::GREATER, ">", std::nullopt, 1),
+              std::make_unique<LiteralExpr>(3.0)
+              )
+          ),
+      Token(TokenType::EQUAL_EQUAL, "==", std::nullopt, 1),
+      std::make_unique<LiteralExpr>("true")
+      );
+
+  ErrorReporter reporter;
+  Scanner scanner(code, reporter);
+  auto tokens = scanner.ScanTokens();
+  ASSERT_EQ(tokens.size(), 8);
+
+  Parser parser(tokens, reporter);
+  ExprPtr expr = parser.Parse();
+  ASSERT_EQ(reporter.status(), LoxStatus::OK);
+
+  AstPrinter printer;
+  ASSERT_EQ(printer.Print(*expr.get()), printer.Print(*expected.get()));
+}
+
+TEST(ParserTest, BasicError) {
+  std::string code = "(3\n";
+  ErrorReporter reporter;
+  Scanner scanner(code, reporter);
+  auto tokens = scanner.ScanTokens();
+  ASSERT_EQ(tokens.size(), 3);
+  Parser parser(tokens, reporter);
+  ExprPtr expr = parser.Parse();
+  ASSERT_EQ(reporter.status(), LoxStatus::ERROR);
+  ASSERT_EQ(expr.get(), nullptr);
 }
 } //namespace cclox
