c941803ce04cfeb35b3144e9d3e9846b38fe77fb
lj970926 <1783973490@qq.com>
2023-01-13 11:37:18 +0800

    feat: add interface for executor

---
+++ include/executor.h
@@ -0,0 +1,22 @@
+//
+// Created by lijin on 23-1-13.
+//
+
+#ifndef CCLOX_EXECUTOR_H
+#define CCLOX_EXECUTOR_H
+#include "expr.h"
+#include "token.h"
+
+namespace cclox {
+class Executor: public Visitor {
+ public:
+  void VisitBinary(const BinaryExpr& expr) override;
+  void VisitGrouping(const GroupingExpr& expr) override;
+  void VisitLiteral(const LiteralExpr& expr) override;
+  void VisitUnary(const UnaryExpr& expr) override;
+ private:
+  OptionalLiteral value_;
+};
+} //namespace cclox
+
+#endif  // CCLOX_EXECUTOR_H
