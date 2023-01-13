9c154ac4e69676f284147dadc99641813317bc06
lj970926 <1783973490@qq.com>
2023-01-13 22:41:17 +0800

    feat: add runtime error

---
+++ include/error.h
@@ -0,0 +1,22 @@
+//
+// Created by lijin on 2023/1/13.
+//
+
+#ifndef CCLOX_ERROR_H
+#define CCLOX_ERROR_H
+#include <stdexcept>
+#include <string>
+
+#include "token.h"
+
+namespace cclox {
+class RuntimeError: public std::runtime_error {
+ public:
+  RuntimeError(Token token, const std::string& message)
+      : std::runtime_error(message.c_str()), token_(token) {}
+ private:
+  Token token_;
+};
+} //namespace cclox
+
+#endif  // CCLOX_ERROR_H
