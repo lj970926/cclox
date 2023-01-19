160ecb2dc2d960bd131a17012a3f0e8d40f40b2f
lj970926 <1783973490@qq.com>
2023-01-19 11:46:47 +0800

    feat: add e2e test class.

--- tools/run_e2e_test.py
+++ tools/run_e2e_test.py
@@ -7,36 +7,77 @@ import re
 EXPECTED_OUPUT_PATTERN = re.compile(r"// expected: ?(.*)")
 EXPECTED_ERROR_PATTERN = re.compile(r"// (Error.*)")
 NON_TEST_PATTERN = re.compile(r"// nontest")
+ERROR_LINE_PATTERN = re.compile(r"// \[((java|c) )?line (\d+)\] (Error.*)")
+EXPECTED_RUNTIME_ERROR_PATTERN = re.compile(r"// expect runtime error: (.+)")
 
 sample_basedir = "sample"
 
 class Test:
-    def __init__(self, expected_outputs=list()) -> None:
-        self.expected_ouputs = expected_outputs
+    def __init__(self, expected_outputs=list(), expected_errors=list(), expected_exit_code=0, expected_runtime_error=None):
+        self.expected_outputs = expected_outputs
+        self.expected_errors = expected_errors
+        self.expected_exit_code = expected_exit_code
+        self.expected_runtime_error = expected_runtime_error
+        self.failures = list()
+    
+    def parse(self, file_path: str, lines: typing.List[str]) -> bool:
+
+        for lineno, line in enumerate(lines):
+            match = NON_TEST_PATTERN.search(line)
+            if match:
+                self.failures.append(f"Skip nontest file {file_path}")
+                return False
+
+            match = EXPECTED_OUPUT_PATTERN.search(line)
+            if match:
+                self.expected_outputs.append({
+                    "line": lineno + 1,
+                    "output": match[1]
+                })
+                continue
+            
+            match = EXPECTED_ERROR_PATTERN.search(line)
+            if match:
+                self.expected_errors.append(f"[line {lineno + 1}] {match[1]}")
+                self.expected_exit_code = 65
+                continue
+
+            match = ERROR_LINE_PATTERN.search(line)
+            if match:
+                self.expected_errors.append(f"[line {match[3]}] {match[4]}")
+                self.expected_exit_code = 65
+                continue
+
+            match = EXPECTED_RUNTIME_ERROR_PATTERN.search(line)
+            if match:
+                self.expected_runtime_error = f"[line {lineno + 1}] {match[1]}"
+                self.expected_exit_code = 70
+        
+        if len(self.expected_errors) > 0 and self.expected_runtime_error:
+            self.failures.append("Can not expect both compile and runtime errors.")
+            return False
+        
+        return True
+    
+    def print_failures(self):
+        for failure in self.failures:
+            print(failure)
+
+    def run(self):
+        pass
 
 
 def run_test(test_path: str):
     lines = list()
     with open(test_path, "r", encoding="utf-8") as fp:
         lines.extend(fp.readlines())
-    if not parse(lines):
+    test = Test()
+
+    if not test.parse(test_path, lines):
+        test.print_failures()
         return
     
-    run(test_path, lines)
-
-def  parse(file_path, lines: typing.List[str]):
-    expected_outputs = list();
-    for line in lines:
-        match = NON_TEST_PATTERN.search(line)
-        if match:
-            print(f"Skip nontest file {file_path}")
-            return False
-        match = EXPECTED_OUPUT_PATTERN.search(line)
-        if match:
-            expected_outputs.append(match[1])
-
-def run(lines):
-    pass
+    test.run()
 
 def main(args):
     cclox_path = args.executable
