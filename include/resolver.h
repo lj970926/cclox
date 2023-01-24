c1f58f95dc99ab925cefc3b1662f2c55fac280a7
lj970926 <1783973490@qq.com>
2023-01-24 22:51:25 +0800

    feat: add resolve interface.

--- include/resolver.h
+++ include/resolver.h
@@ -5,12 +5,21 @@
 #ifndef CCLOX_RESOLVER_H
 #define CCLOX_RESOLVER_H
 
+#include <vector>
+
 #include "expr.h"
 #include "stmt.h"
 
 namespace cclox {
 class Resolver: public ExprVisitor, public StmtVisitor {
-
+ public:
+  void VisitBlockStmt(const BlockStmt& stmt) override;
+ private:
+  void BeginScope();
+  void EndScope();
+  void Resolve(const std::vector<StmtPtr>& stmts);
+  void Resolve(const Expr& expr);
+  void Resolve(const Stmt& stmt);
 };
 } //namespace cclox
 
--- src/resolver.cpp
+++ src/resolver.cpp
@@ -2,3 +2,21 @@
 // Created by lijin on 23-1-24.
 //
 #include "resolver.h"
+
+namespace cclox {
+void Resolver::VisitBlockStmt(const BlockStmt &stmt) {
+  BeginScope();
+  for (const auto& sub_stmt: stmt.stmts) {
+    Resolve(*sub_stmt);
+  }
+  EndScope();
+}
+
+void Resolver::Resolve(const Stmt &stmt) {
+  stmt.Accept(*this);
+}
+
+void Resolver::Resolve(const Expr &expr) {
+  expr.Accept(*this);
+}
+}
